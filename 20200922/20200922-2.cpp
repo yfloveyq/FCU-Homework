a877568fcbf95c084b9080fa1cdfab9fe63cbcb1
Phil Wu <philwu53@gmail.com>
2020-09-28 01:21:26 +0800

    correcting last part

--- 20200922/20200922-2.cpp
+++ 20200922/20200922-2.cpp
@@ -1,29 +1,29 @@
-#include <stdio.h>
-#include <math.h>
-unsigned long long int sum = 0;
-unsigned long long int chess(unsigned long long int num, unsigned long long int counter);
+#include <cstdio>
+#include <cmath>
+
+int chess(unsigned long long int num, unsigned long long int counter);
 
 int main() {
 
-    unsigned long long int num, counter = 0;
+    unsigned long long int num;
+    unsigned long long int counter = 0;
+
     scanf("%llu", &num);
-    printf("%llu", chess(num, counter));
+    printf("%d", chess(num, counter));
 
     return 0;
 }
 
-unsigned long long int chess(unsigned long long int num, unsigned long long int counter) {
+int chess(unsigned long long num, unsigned long long int counter) {
 
     if(num == 0){
         return 0;
     }
-    if(num >= pow(2, 63)){
-        return 64;
-    }
 
-    sum += pow(2,counter);
-    if(sum < num){
-        return chess(num, counter+1);
+    unsigned long long int sum = pow(2,(double )counter);
+
+    if(num > sum){
+        return chess(num-sum, counter+1);
     }else{
         return counter+1;
     }
